8050d898c879190945f454dd418ae8ed45619074
skmara2 <skmara2@inbox.lv>
2020-11-13 15:44:49 +0200

    mapes

---
+++ src/pirmais.py
@@ -0,0 +1,12 @@
+"""Funkcija akceptē divus argumentus - trisjtūra augstumu un pamatu,
+aprēķina trijstūra laukumu un atgriež to. Ja kāds no argumentiem ir mazāks vai vienāds ar 0, atgriež 0.
+Formula trijstūra laukuma aprēķināšanai ir augstums*pamats/2
+
+Argumenti:
+    h {int vai float} -- trijstūra augstums
+    p {int vai float} -- trijstūra pamats
+Atgriež:
+    int vai float -- rezultāts"""
+
+def laukums(h,p):
+    
